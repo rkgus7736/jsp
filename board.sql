0a220d2e08396e43903dd11e532e2cc03ad66482
rkgus7736 <rkgus7736@nate.com>
2021-02-01 17:01:18 +0900

    2

---
+++ board.sql
@@ -0,0 +1,30 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-1월-11-2021   
+--------------------------------------------------------
+REM INSERTING into SCOTT.BOARD
+SET DEFINE OFF;
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'TEST',to_date('21/01/05','RR/MM/DD'),26,'admin','내용이다',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'zzzzzzzzzzzzzzzzzzz',to_date('21/01/06','RR/MM/DD'),20,'admin','zzzzzzzzzzzzzzzzzzz',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'바보',to_date('21/01/06','RR/MM/DD'),3,'admin','멍청이',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'바보',to_date('21/01/06','RR/MM/DD'),2,'admin','멍청이',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'바보',to_date('21/01/06','RR/MM/DD'),10,'admin','ㅁ',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'fghfgh',to_date('21/01/06','RR/MM/DD'),35,'admin','gfgggg',20,205);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'asd',to_date('21/01/06','RR/MM/DD'),1,'admin','619645
+',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'asdasd',to_date('21/01/06','RR/MM/DD'),6,'admin','1456632',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'abc',to_date('21/01/06','RR/MM/DD'),1,'admin','.',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'룰루룰루루',to_date('21/01/06','RR/MM/DD'),6,'admin','야호',2,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'왜안됭',to_date('21/01/06','RR/MM/DD'),3,'admin','?',17,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'cbvcvb',to_date('21/01/07','RR/MM/DD'),0,'admin','we235wte',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'13',to_date('21/01/07','RR/MM/DD'),0,'123456','14',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'gsdfg',to_date('21/01/08','RR/MM/DD'),0,'admin','sdfgsdfg',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'mnmmm',to_date('21/01/08','RR/MM/DD'),0,'admin','mmmmm',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'bbbb',to_date('21/01/11','RR/MM/DD'),0,'admin','bbbbbbbb',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'bbbb',to_date('21/01/11','RR/MM/DD'),0,'admin','bbbbbbbb',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'bbbb',to_date('21/01/11','RR/MM/DD'),0,'admin','bbbbbbbb',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'bbbb',to_date('21/01/11','RR/MM/DD'),0,'admin','bbbbbbbb',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'nm,n,nm,',to_date('21/01/11','RR/MM/DD'),0,'admin','nm,nm,n',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'vvvv',to_date('21/01/11','RR/MM/DD'),0,'admin','vvvvvv',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'어려웡ㅠ',to_date('21/01/11','RR/MM/DD'),1,'admin','아냐 할수 있어!',1,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'히히히',to_date('21/01/11','RR/MM/DD'),0,'admin','ㅎㅎㅎ',0,0);
+Insert into SCOTT.BOARD (BNO,TITLE,BDATE,BCOUNT,WRITER,CONTENT,BLIKE,BHATE) values (bno_seq.nextval,'실험',to_date('21/01/11','RR/MM/DD'),0,'admin','어디보자',0,0);
